518b1cc3c97934591c2d58b8834a800725844e0d
XORGeek New <xornew@Sohel-MacBook-Air.local>
2022-03-16 19:33:59 +0600

    Shimmer Effect not completed

--- Application (JSON Parsing)/Application (JSON Parsing)/ViewController.swift
+++ Application (JSON Parsing)/Application (JSON Parsing)/ViewController.swift
@@ -11,9 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        parseJSON()
     }
-
-
+    private func parseJSON() {
+        guard let path = Bundle.main.path(forResource: "JSONData", ofType: "json")
+        else {
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        
+        var output: Output?
+        do {
+            let jsonData = try Data(contentsOf: url)
+            output = try JASONDecoder()
+        }
+        catch {
+            print("Error \(error)")
+        }
+    }
 }
 
+struct Output: Codable {
+    let data: [OutputItem]
+}
+struct OutputItem: Codable {
+    let title: String
+    let items: [String]
+}
